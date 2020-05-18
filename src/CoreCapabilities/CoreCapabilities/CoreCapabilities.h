f187be9a888fac3baf0bde6ab0e5fe5a4a1b195f
Ilya Kremniou <cvobonde@yandex.ru>
2020-05-18 06:35:15 +0300

    Review

--- src/CoreCapabilities/CoreCapabilities/CoreCapabilities.h
+++
@@ -1,8 +0,0 @@
-﻿// CoreCapabilities.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
